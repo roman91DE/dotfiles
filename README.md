49f999255981b56b4a29c66bf5032f206e891c68
Roman Hoehn <rohoehn123@gmail.com>
2023-04-01 15:55:19 +0200

    rewrote symlinker script and changed directory structure

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # My personal dotfile repository
 
-Setup based on [Link](https://www.atlassian.com/git/tutorials/dotfiles)
+## Usage
 
+Clone this repository and run the `symlinker.py` script to create symlinks to all configuration files. 
+
+**Warning: This will overwrite any existing configuration files so make sure to do a backup before running `symlink.py`**  
 
--- symlinker.py
+++ symlinker.py
@@ -3,30 +3,43 @@
 from subprocess import run
 from os import listdir, path
 from typing import List
-from sys import stdout, stderr
+from sys import stdout, stderr, exit
 
 def main():
 
-    print(f"Starting to create Symbolic Links for all dotfiles")
+    def safetyCheck():
+        print(f"Warning: Running this script will overwrite all existing configuration files!", file=stderr)
+        return input("Type yes to continue: ")
+
+
+    if safetyCheck() != "yes":
+        print("Aborted Process", file=stderr)
+        exit()
+
+    
+    print(f"Starting to create Symbolic Links...\n---")
 
     HOME_DIR = path.expanduser("~")
-    CONFIG_DIR = "./dotfiles"
+    REPO = "dotfiles"
+    CONFIG_DIR = "dotfile-directory"
     CONFIGFILES: List[str] = listdir(CONFIG_DIR)
 
     for file in CONFIGFILES:
 
-
-
-        print(f"Linking File/Folder: {file}")
+        print(f"Linking <{file}> with:")
 
         CMD = [
             "ln",
             "-s",
             "-F",
             "-f",
-            path.join(CONFIG_DIR, file),
+            path.join(HOME_DIR, REPO, CONFIG_DIR, file),
             path.join(HOME_DIR, file),
         ]
+
+        for token in CMD:
+            print(token, end=" ")
+        print()
 
         result = run(
             CMD,
