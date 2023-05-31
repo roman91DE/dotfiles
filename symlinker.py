e15fb0c0f01dafb1f3583764704ca671883cb251
Roman Hoehn <rohoehn123@gmail.com>
2023-05-31 11:00:07 +0200

    refactored code

--- symlinker.py
+++ symlinker.py
@@ -1,69 +1,111 @@
 #!/usr/bin/env python3
 
-from subprocess import run
-from os import listdir, path
-from typing import List
-from sys import stdout, stderr, exit
+import os
+import sys
+from datetime import datetime
 
 
-def safety_check():
-    """ warn user to avoid the loss of existing dot files """
-    print(f"Warning: Running this script will overwrite all existing configuration files!", file=stderr)
-    if input("Type yes to continue: ") != "yes":
-        print("Aborted Process", file=stderr)
-        exit()
+class Utils:
+    """Utility functions for the symlinker"""
 
+    paths = {
+        "home": os.path.expanduser("~"),
+        "repo": os.path.dirname(__file__),
+        "config_src": os.path.join(os.path.dirname(__file__), "configfiles"),
+    }
 
-def prepend_dot(file: str) -> str:
-        """ Prepends a dot to filenames """
-        if file.startswith("."):
-            return file
+    uids = {
+        "user": os.getuid(),
+    }
+
+    @staticmethod
+    def prepend_dot(filename: str) -> str:
+        """Prepends a dot to filenames"""
+        if filename.startswith("."):
+            return filename
         else:
-            return f".{file}"
+            return f".{filename}"
+
+    @staticmethod
+    def safety_check():
+        """warn user to avoid the loss of existing dot files"""
+        while True:
+            print(
+                f"Warning: Running this script will overwrite all existing configuration files!",
+                file=sys.stderr,
+            )
+            answer = input("Type yes to continue, or no to abort: ")
+            if answer.lower() == "yes":
+                break
+            elif answer.lower() == "no":
+                print("Aborted Process", file=sys.stderr)
+                sys.exit()
+            else:
+                print("Invalid input. Please enter 'yes' or 'no'.", file=sys.stderr)
+
+    @staticmethod
+    def backup_existing_files() -> bool:
+        """ask user if they want to backup their existing dot files"""
+        while True:
+            print(
+                f"Do you want to backup any configuration files that already exist?",
+                file=sys.stdout,
+            )
+            answer = input("Type yes to continue, or no to abort: ")
+            if answer.lower() == "yes":
+                return True
+            elif answer.lower() == "no":
+                return False
+            else:
+                print("Invalid input. Please enter 'yes' or 'no'.", file=sys.stderr)
 
 
 def main():
+    Utils.safety_check()
+    create_backups = Utils.backup_existing_files()
 
-    safety_check()
-    
     print(f"Starting to create Symbolic Links...\n---")
 
-    HOME_DIR = path.expanduser("~")
-    REPO = "dotfiles"
-    CONFIG_DIR = "dotfile-directory"
-    CONFIGFILES: List[str] = listdir(CONFIG_DIR)
+    configfiles = os.listdir(Utils.paths["config_src"])
 
-    
+    for file in configfiles:
+        file_path = os.path.join(Utils.paths["config_src"], file)
+        link_path = os.path.join(Utils.paths["home"], Utils.prepend_dot(file))
 
-    for file in CONFIGFILES:
+        # handle existing files
+        if os.path.exists(link_path):
+            # skip if file is not owned by user
+            if os.stat(link_path).st_uid != Utils.uids["user"]:
+                print(
+                    f"Skipping {link_path} as it is not owned by the user {os.getlogin()}",
+                    file=sys.stderr,
+                )
+                continue
 
-        command = [
-            "ln",
-            "-s",
-            "-F",
-            "-f",
-            path.join(HOME_DIR, REPO, CONFIG_DIR, file),
-            path.join(HOME_DIR, prepend_dot(file)),
-        ]
+            # remove existing symbolic links
+            elif os.path.islink(link_path):
+                os.remove(link_path)
+                print(
+                    f"Replacing {link_path} as it is is already a symbolic link",
+                    file=sys.stderr,
+                )
+                continue
 
-        print(f"Linking configuration file <{file}> with:")
+            # backup existing files if option selected
+            elif (not os.path.islink(link_path)) and create_backups:
+                backup_path = os.path.join(
+                    Utils.paths["home"],
+                    f"{file}.bak-{datetime.now().strftime('%Y%m%d%H%M%S')}",
+                )
+                os.rename(link_path, backup_path)
+                print(f"Backed up {link_path} to {backup_path}", file=sys.stderr)
 
-        for token in command:
-            print(token, end=" ")
-        print()
-
-        result = run(
-            command,
-            capture_output=True,
-            text=True
-        )
-
-        print(result.stdout, file=stdout)
-        print(result.stderr, file=stderr)
-
+        try:
+            os.symlink(file_path, link_path)
+            print(f"Created symbolic link for {file} in {link_path}")
+        except Exception as e:
+            print(f"Failed to create symbolic link for {file}: {e}", file=sys.stderr)
 
 
 if __name__ == "__main__":
     main()
-
-    
