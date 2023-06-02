e525254442666baf463f4e20bc93ae9ec953adaa
Roman Hoehn <rohoehn123@gmail.com>
2023-06-02 14:43:17 +0200

    added abbr for software updates

--- configfiles/config/fish/config.fish
+++ configfiles/config/fish/config.fish
@@ -24,6 +24,19 @@ set fish_greeting "Using Non-Posix compliant Fish Shell"
 
 # abbreviations
 
+# update software from packet managers
+
+abbr -a -g apt-up "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y&& sudo apt clean -y"
+abbr -a -g snap-up "sudo snap refresh"
+# abbr -a -g pacman-up "sudo pacman -Syu"
+# abbr -a -g yay-up "yay -Syu"
+abbr -a -g brew-up "brew update && brew upgrade && brew cleanup"
+abbr -a -g conda-up "conda update --all"
+abbr -a -g pip-up "pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
+abbr -a -g pkg-up "pkg update && pkg upgrade"
+
+
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 
