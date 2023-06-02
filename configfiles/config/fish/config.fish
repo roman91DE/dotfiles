ee686ee50e8dbf247a25e418fbd888156f0588b6
Roman Hoehn <rohoehn123@gmail.com>
2023-06-02 15:31:50 +0200

    rewrote config.fish

--- configfiles/config/fish/config.fish
+++ configfiles/config/fish/config.fish
@@ -2,44 +2,48 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# adding PATHs
+# show neofetch at startup
+neofetch
 
-# homebrew on apple silicone
+
+# macOS specific setup
 switch (uname)
     case Darwin
-            fish_add_path /opt/homebrew/bin
-    case '*'
-            echo Skipped MacOs Setup
+            set -gx fish_user_paths /opt/homebrew/bin
+            abbr -a -g brew-up "brew update && brew upgrade && brew cleanup"
+            echo MacOs Setup            
 end
 
-# custom Scripts folder
+# Linux specific setup
+switch (uname)
+    case Linux
+            abbr -a -g apt-up "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y&& sudo apt clean -y"
+            abbr -a -g snap-up "sudo snap refresh"
+            abbr -a -g pacman-up "sudo pacman -Syu"
+            abbr -a -g yay-up "yay -Syu"
+            abbr -a -g pkg-up "pkg upgrade"
+            echo Linux Setup
+end
 
-fish_add_path ~/Scripts
+# (python) - pip specific setup
+if command -v pip > /dev/null
+    abbr -a -g pip-up "pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
+end
+
+
+# add custom Scripts folder to path
+for dir in ~/Scripts ~/scripts 
+    if test -d $dir
+        set -gx fish_user_paths $dir
+    end
+end
+
+
 
 # vi mode
 fish_vi_key_bindings
 
-# custom greeting
-set fish_greeting "Using Non-Posix compliant Fish Shell"
-
-# abbreviations
-
-# update software from packet managers
-
-abbr -a -g apt-up "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y&& sudo apt clean -y"
-abbr -a -g snap-up "sudo snap refresh"
-# abbr -a -g pacman-up "sudo pacman -Syu"
-# abbr -a -g yay-up "yay -Syu"
-abbr -a -g brew-up "brew update && brew upgrade && brew cleanup"
-abbr -a -g conda-up "conda update --all"
-abbr -a -g pip-up "pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
-abbr -a -g pkg-up "pkg update && pkg upgrade"
-
-
-
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-
 # modified to work with different user names and conda versions
 set -l USERS rmn roman
 set -l CONDAS miniconda anaconda
@@ -51,14 +55,15 @@ for USER in $USERS
             set -l path /Users/$USER/$CONDA$VERSION/bin/conda
             if test -f $path
                 eval $path "shell.fish" "hook" $argv | source
-                break 3
+                abbr -a -g conda-up "conda update --all"
+                break
             end
         end
     end
 end
-
-
 # <<< conda initialize <<<
 
-# show neofetch at startup
-neofetch
+# use a custom fish greeting that shows the version number
+function fish_greeting
+    echo "fish version: "(fish --version | cut -d " " -f 3)
+end
