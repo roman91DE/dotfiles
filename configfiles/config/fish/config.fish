d1daf1bdd4f94006460057b64e200c04010a652a
Roman Hoehn <rohoehn123@gmail.com>
2023-06-03 10:50:47 +0200

    included termux in config.fish

--- configfiles/config/fish/config.fish
+++ configfiles/config/fish/config.fish
@@ -1,33 +1,38 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+
+    # show neofetch at startup
+    neofetch
+
+    # vi mode
+    fish_vi_key_bindings
+
+    # use a custom fish greeting that shows the version number
+    function fish_greeting
+        echo "fish version: "(fish --version | cut -d " " -f 3)
+    end
+
 end
 
-# show neofetch at startup
-neofetch
 
 
-# macOS specific setup
+# Linux specific setup
 switch (uname)
     case Darwin
             set -gx fish_user_paths /opt/homebrew/bin
             abbr -a -g brew-up "brew update && brew upgrade && brew cleanup"
             echo MacOs Setup            
-end
-
-# Linux specific setup
-switch (uname)
     case Linux
 	    switch (uname -o)
 		    case Android
-			abbr -a -g pkg-up "pkg upgrade"
-			echo Termux Setup
-		end
-	case (*)
-            abbr -a -g apt-up "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y&& sudo apt clean -y"
-            abbr -a -g snap-up "sudo snap refresh"
-            abbr -a -g pacman-up "sudo pacman -Syu"
-            abbr -a -g yay-up "yay -Syu"
-                        echo Linux Setup
+                abbr -a -g pkg-up "pkg upgrade"
+                echo Termux Setup
+	        case (*)
+                abbr -a -g apt-up "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y&& sudo apt clean -y"
+                abbr -a -g snap-up "sudo snap refresh"
+                abbr -a -g pacman-up "sudo pacman -Syu"
+                abbr -a -g yay-up "yay -Syu"
+                echo Linux Setup
+        end
 end
 
 # (python) - pip specific setup
@@ -44,9 +49,6 @@ for dir in ~/Scripts ~/scripts
 end
 
 
-
-# vi mode
-fish_vi_key_bindings
 
 # >>> conda initialize >>>
 # modified to work with different user names and conda versions
@@ -68,7 +70,3 @@ for USER in $USERS
 end
 # <<< conda initialize <<<
 
-# use a custom fish greeting that shows the version number
-function fish_greeting
-    echo "fish version: "(fish --version | cut -d " " -f 3)
-end
