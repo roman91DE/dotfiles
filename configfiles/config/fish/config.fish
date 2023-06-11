8526cebd404f9e58dd951108b460fe92f7b9c014
Roman Hoehn <rohoehn123@gmail.com>
2023-06-11 19:08:45 +0200

    2023-06-11_19:08:45

--- configfiles/config/fish/config.fish
+++ configfiles/config/fish/config.fish
@@ -10,7 +10,7 @@ if status is-interactive
             switch (uname -o)
                 case Android    # Termux/Linux Emulator
                         abbr -a -g ,pkg-up "pkg upgrade"
-			abbr -a -g ,run-fedora "'proot-distro login fedora"
+			abbr -a -g ,run-fedora "proot-distro login fedora"
                         echo Termux Setup
                     case '*'    # Linux
                         abbr -a -g ,apt-up "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y&& sudo apt clean -y"
